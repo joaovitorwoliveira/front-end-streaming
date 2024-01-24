1392bdcdc7a56157cc9dfb381a6a60558d476534
Joao Vitor Oliveira <joaovitorwoliveira@gmail.com>
2024-01-24 17:43:16 -0300

    Configuração finalizada da nossa seção de destaque, fazendo conexão com o backend, criando mais um método para o serviço de cursos, criando a sua responsividade - Seção destaque

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -26,6 +26,23 @@ const courseService = {
 
     return res;
   },
+  getFeaturedCouses: async () => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .get("/courses/featured", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+
+        return error.response;
+      });
+
+    return res;
+  },
 };
 
 export default courseService;
