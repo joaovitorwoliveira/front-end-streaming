27eede1bf3f351797b3e3ae9a1fa297b8388ec65
Joao Vitor Oliveira <joaovitorwoliveira@gmail.com>
2024-01-31 15:01:03 -0300

    Criação dos elementos de formulário de senha e do seu estilo visual - Formulário de senha

---
+++ src/components/profile/password/index.tsx
@@ -0,0 +1,62 @@
+import styles from "../../../../styles/profile.module.scss";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+
+const PasswordForm = function () {
+  return (
+    <>
+      <Form className={styles.form}>
+        <div className={styles.inputNormalDiv}>
+          <FormGroup>
+            <Label className={styles.label} for="currentPassword">
+              SENHA ATUAL
+            </Label>
+            <Input
+              name="currentPassword"
+              type="password"
+              id="currentPassword"
+              placeholder="******"
+              required
+              minLegth={6}
+              maxLength={12}
+              className={styles.input}
+            />
+          </FormGroup>
+        </div>
+        <div className={styles.inputFlexDiv}>
+          <FormGroup>
+            <Label className={styles.label} for="newPassword">
+              NOVA SENHA
+            </Label>
+            <Input
+              name="newPassword"
+              type="password"
+              id="newPassword"
+              placeholder="******"
+              required
+              className={styles.inputFlex}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label className={styles.label} for="confirmNewPassword">
+              CONFIRMAR NOVA SENHA
+            </Label>
+            <Input
+              name="confirmNewPassword"
+              type="password"
+              id="confirmNewPassword"
+              placeholder="******"
+              required
+              className={styles.inputFlex}
+            />
+          </FormGroup>
+
+          <Button className={styles.formBtn} outline>
+            Salvar Alterações
+          </Button>
+        </div>
+      </Form>
+    </>
+  );
+};
+
+export default PasswordForm;
