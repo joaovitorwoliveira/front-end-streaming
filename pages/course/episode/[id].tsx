0c3ebba7661079ba31a268837ca718c37ce92966
Joao Vitor Oliveira <joaovitorwoliveira@gmail.com>
2024-02-05 16:05:47 -0300

    Inicio do método de tempo para a pagina de episódios (nao finalizada)

--- pages/course/episode/[id].tsx
+++ pages/course/episode/[id].tsx
@@ -10,12 +10,36 @@ import { Container, Button } from "reactstrap";
 import courseService, { CourseType } from "@/src/services/courseService";
 import PageSpinner from "@/src/components/common/spinner";
 import ReactPlayer from "react-player";
+import watchEpisodeService from "@/src/services/episodeService";
 
 const EpisodePlayer = function () {
   const router = useRouter();
   const [course, setCourse] = useState<CourseType>();
   const episodeOrder = parseFloat(router.query.id?.toString() || "");
+  const episodeId = parseFloat(router.query.episodeid?.toString() || "");
+
   const courseId = router.query.courseid?.toString() || "";
+
+  const [getEpisodeTime, setGetEpisodeTime] = useState(0);
+  const [episodeTime, setEpisodeTime] = useState(0);
+
+  const handleGetEpisodeTime = async () => {
+    const res = await watchEpisodeService.getWatchTime(episodeId);
+    // alteracao 01 inicio
+    console.log("Episode ID:", episodeId);
+    console.log("Response:", res);
+    //alteracao 01 fim
+
+    console.log(res);
+
+    if (res.data !== null) {
+      setGetEpisodeTime(res.data.seconds);
+    }
+  };
+
+  useEffect(() => {
+    handleGetEpisodeTime();
+  }, [router]);
 
   const getCourse = async function () {
     if (typeof courseId !== "string") return;
@@ -27,11 +51,19 @@ const EpisodePlayer = function () {
   };
 
   const handleLastEpisode = () => {
-    router.push(`/course/episode/${episodeOrder - 1}?courseid=${course?.id}`);
+    router.push(
+      `/course/episode/${episodeOrder - 1}?courseid=${
+        course?.id
+      }&episodeid=${episodeId}`
+    );
   };
 
   const handleNextEpisode = () => {
-    router.push(`/course/episode/${episodeOrder + 1}?courseid=${course?.id}`);
+    router.push(
+      `/course/episode/${episodeOrder + 1}?courseid=${course?.id}&episodeid=${
+        episodeId + 1
+      }`
+    );
   };
 
   useEffect(() => {
